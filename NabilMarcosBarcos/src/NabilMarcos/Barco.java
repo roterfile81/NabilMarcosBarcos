03be6e5a8543fb41e0472da787223307913bb525
HamzaA900 <1DAM@A11-PC10>
2026-01-19 11:57:00 +0100

    nuevo

--- NabilMarcosBarcos/src/NabilMarcos/Barco.java
+++ NabilMarcosBarcos/src/NabilMarcos/Barco.java
@@ -1,6 +1,34 @@
 package NabilMarcos;
 
-public class Barco {
-	
-	
+public abstract class Barco {
+	protected String nombre;
+    protected Motor motor;
+    protected double capacidadCombustible;
+
+    public Barco(String nombre, Motor motor, double capacidadCombustible) {
+        this.nombre = nombre;
+        this.motor = motor;
+        this.capacidadCombustible = capacidadCombustible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public double getCapacidadCombustible() {
+        return capacidadCombustible;
+    }
+
+    public abstract double calcularAutonomia();
+
+    @Override
+    public String toString() {
+        return "Barco [nombre=" + nombre + ", motor=" + motor +
+               ", capacidadCombustible=" + capacidadCombustible + "]";
+    }
 }
+
---
+++ NabilMarcosBarcos/src/NabilMarcos/puerto.java
@@ -0,0 +1,10 @@
+package NabilMarcos;
+
+public class puerto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
